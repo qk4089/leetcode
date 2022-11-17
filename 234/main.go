0872fc5e0ce477ad6e66f29a3820e55ed4f040f5
Kun Qian <kun.qian@zoom.us>
2022-11-17 18:16:23 +0800

    feat: 2022-11-17
    Signed-off-by: Kun Qian <kun.qian@zoom.us>

---
+++ 234/main.go
@@ -0,0 +1,34 @@
+package _234
+
+//给你一个单链表的头节点head，请你判断该链表是否为回文链表。如果是，返回true；否则，返回false。
+
+//提示：
+//	链表中节点数目在范围[1, 105] 内
+//	0 <= Node.val <= 9
+
+//进阶：你能否用 O(n) 时间复杂度和 O(1) 空间复杂度解决此题？
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func main(head *ListNode) bool {
+	var result *ListNode
+	fast, slow := head, head
+	for fast != nil && fast.Next != nil {
+		fast = fast.Next.Next
+		tmp := slow
+		slow, tmp.Next, result = slow.Next, result, tmp
+	}
+	if fast != nil {
+		slow = slow.Next
+	}
+	for slow != nil {
+		if result.Val != slow.Val {
+			return false
+		}
+		result, slow = result.Next, slow.Next
+	}
+	return true
+}
---
+++ 234/main_test.go
@@ -0,0 +1,16 @@
+package _234
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	a := assert.New(t)
+	l1 := &ListNode{1, nil}
+	a.True(main(l1))
+	l2 := &ListNode{1, &ListNode{2, &ListNode{2, &ListNode{1, nil}}}}
+	a.True(main(l2))
+	l3 := &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{2, &ListNode{1, nil}}}}}
+	a.True(main(l3))
+}
