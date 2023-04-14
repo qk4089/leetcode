230473c8904ca2893af3d1e07fbb0c7f04aae36e
Kun Qian <kun.qian@zoom.us>
2023-04-14 11:05:58 +0800

    feat: 2023-04-14
    Signed-off-by: Kun Qian <kun.qian@zoom.us>`
    Signed-off-by: Kun Qian <kun.qian@zoom.us>

--- misc/misc_test.go
+++ misc/misc_test.go
@@ -38,3 +38,17 @@ func Test_b_search_last(t *testing.T) {
 func Test_findQueens(t *testing.T) {
 	findQueens(0, make([]int, 8))
 }
+
+func Test_beibao(t *testing.T) {
+	w, items := 9, []int{2, 2, 4, 6, 3}
+	fmt.Println(recall(w, items))
+	fmt.Println(dynamic(w, items))
+	fmt.Println(dynamic2(w, items))
+}
+
+func Test_beibao_with_price(t *testing.T) {
+	w, items, values := 9, []int{2, 2, 4, 6, 3}, []int{3, 4, 8, 9, 6}
+	fmt.Println(dynamic_with_price(w, items, values))
+	//fmt.Println(dynamic_with_price2(w, items, values))
+	fmt.Println(dynamic_with_price3(w, items, values))
+}
