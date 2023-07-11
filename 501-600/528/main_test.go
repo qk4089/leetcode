a624da10b6c37884668cc9e46dce7b5a72632c49
Kun Qian <kun.qian@zoom.us>
2023-07-11 17:52:20 +0800

    feat: 2023-07-12

---
+++ 501-600/528/main_test.go
@@ -0,0 +1,15 @@
+package _528
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	s := Constructor([]int{1})
+	fmt.Println(s.PickIndex())
+	fmt.Println(s.PickIndex())
+	fmt.Println(s.PickIndex())
+	fmt.Println(s.PickIndex())
+	fmt.Println(s.PickIndex())
+}
