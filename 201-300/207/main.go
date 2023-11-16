9bb5b09585f3117a803c364a8b850ccb0905b231
Kun Qian <kun.qian@zoom.us>
2023-11-16 09:55:12 +0800

    feat: 2023-11-16
    Signed-off-by: Kun Qian <kun.qian@zoom.us>

---
+++ 201-300/207/main.go
@@ -0,0 +1,73 @@
+package _207
+
+//https://leetcode.cn/problems/course-schedule/description/
+//你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。
+//在选修某些课程之前需要一些先修课程。 先修课程按数组 prerequisites 给出，其中 prerequisites[i] = [ai, bi] ，
+//表示如果要学习课程 ai 则 必须 先学习课程  bi 。
+
+//例如，先修课程对 [0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
+//请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。
+
+//示例 1：
+//输入：numCourses = 2, prerequisites = [[1,0]]
+//输出：true
+//解释：总共有 2 门课程。学习课程 1 之前，你需要完成课程 0 。这是可能的。
+
+//输入：numCourses = 2, prerequisites = [[1,0],[0,1]]
+//输出：false
+//解释：总共有 2 门课程。学习课程 1 之前，你需要先完成课程 0 ；并且学习课程 0 之前，你还应先完成课程 1 。这是不可能的。
+
+//提示：
+//	1 <= numCourses <= 2000
+//	0 <= prerequisites.length <= 5000
+//	prerequisites[i].length == 2
+//	0 <= ai, bi < numCourses
+//	prerequisites[i] 中的所有课程对 互不相同
+
+func canFinish(numCourses int, prerequisites [][]int) bool {
+	courseMap := make(map[int][]int)
+	for _, prerequisite := range prerequisites {
+		if val, ok := courseMap[prerequisite[0]]; ok {
+			courseMap[prerequisite[0]] = append(val, prerequisite[1:]...)
+		} else {
+			courseMap[prerequisite[0]] = append([]int{}, prerequisite[1:]...)
+		}
+	}
+	selected, judged := make(map[int]bool), make(map[int]bool)
+	for course := range courseMap {
+		if judged[course] {
+			continue
+		}
+		if findSelf(course, selected, judged, courseMap) {
+			return false
+		}
+	}
+	return true
+}
+
+func findSelf(course int, selected, judged map[int]bool, courseMap map[int][]int) bool {
+	if judged[course] {
+		return false
+	}
+	if selected[course] {
+		return true
+	}
+	find := false
+	if require, ok := courseMap[course]; ok {
+		selected[course] = true
+		for _, val := range require {
+			if judged[val] {
+				continue
+			}
+			if findSelf(val, selected, judged, courseMap) {
+				find = true
+				break
+			}
+		}
+		delete(selected, course)
+	}
+	if !find {
+		judged[course] = true
+	}
+	return find
+}
