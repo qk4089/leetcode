b27e1e35121c1fcc12d3e3036a8df226d6dbb9dd
Kun Qian <kun.qian@zoom.us>
2024-01-07 14:58:41 +0800

    feat: 2024-01-07
    Signed-off-by: Kun Qian <kun.qian@zoom.us>

---
+++ 101-200/200/main.go
@@ -0,0 +1,53 @@
+package _200
+
+//https://leetcode.cn/problems/number-of-islands/
+//给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+//岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+//此外，你可以假设该网格的四条边均被水包围。
+
+//示例
+//输入：grid = [
+//["1","1","1","1","0"],
+//["1","1","0","1","0"],
+//["1","1","0","0","0"],
+//["0","0","0","0","0"]
+//]
+//输出：1
+
+//输入：grid = [
+//["1","1","0","0","0"],
+//["1","1","0","0","0"],
+//["0","0","1","0","0"],
+//["0","0","0","1","1"]
+//]
+//输出：3
+
+//提示：
+//	m == grid.length
+//	n == grid[i].length
+//	1 <= m, n <= 300
+//	grid[i][j] 的值为 '0' 或 '1'
+
+func numIslands(grid [][]byte) int {
+	cnt := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if grid[i][j] == '1' {
+				dfs(grid, i, j)
+				cnt++
+			}
+		}
+	}
+	return cnt
+}
+
+func dfs(grid [][]byte, x, y int) {
+	if x < 0 || x >= len(grid) || y < 0 || y >= len(grid[0]) || grid[x][y] == '0' {
+		return
+	}
+	grid[x][y] = '0'
+	dfs(grid, x, y-1)
+	dfs(grid, x, y+1)
+	dfs(grid, x-1, y)
+	dfs(grid, x+1, y)
+}
